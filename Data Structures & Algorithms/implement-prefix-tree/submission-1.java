5ff502f3634e4fa9aee28d0919b1952b3742e0cb
FhebReker <146902117+FhebReker@users.noreply.github.com>
2026-05-26 12:28:46 -0400

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.java
@@ -0,0 +1,72 @@
+class PrefixTree {
+    // Use the TrieNode class to maintain structure
+    private TrieNode head;
+    public PrefixTree() {
+        this.head = new TrieNode();
+    }
+
+    public void insert(String word) {
+        // Check from the head, looping through all the letters in the key
+        TrieNode x = head;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            // If there isn't a TrieNode at the index/char, make a new one, then move to it
+            if (x.children[index] == null) {
+                x.children[index] = new TrieNode();
+            }
+            x = x.children[index];
+        }
+
+        // Set this point to be the end of a word
+        x.endOfWord = true;
+    }
+    
+    public boolean search(String word) {
+        // Check from the head, looping through all the letters in the key
+        TrieNode x = head;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            // If the next character isn't already in the structure, the key isn't in the structure
+            if (x.children[index] == null) return false;
+            // Otherwise move onto the next TrieNode
+            else x = x.children[index];
+        }
+
+        // Return whether or not it's the end of a word
+        return x.endOfWord;
+    }
+    
+    public boolean startsWith(String prefix) {
+        // Check from the head, looping through all the letters in the key
+        TrieNode x = head;
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+
+            // If the next character isn't already in the structure, the key isn't in the structure
+            if (x.children[index] == null) return false;
+            // Otherwise move onto the next TrieNode
+            else x = x.children[index];
+        }
+
+        // Return true since the starting part is there
+        return true;
+    }
+}
+class TrieNode {
+    TrieNode[] children;
+    boolean endOfWord;
+    public TrieNode() {
+        children = new TrieNode[26];
+        endOfWord = false;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
