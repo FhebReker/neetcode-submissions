e90977f7e2d99d92547d3b0477df894b981c42c9
FhebReker <146902117+FhebReker@users.noreply.github.com>
2026-05-31 19:06:02 -0400

    Add: dynamicArray - submission-0

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.java
@@ -0,0 +1,48 @@
+class DynamicArray {
+    private int[] array;
+    private int length;
+    private int capacity;
+
+    public DynamicArray(int capacity) {
+        this.array = new int[capacity];
+        this.length = 0;
+        this.capacity = capacity;
+    }
+
+    public int get(int i) {
+        return array[i];
+    }
+
+    public void set(int i, int n) {
+        array[i] = n;
+    }
+
+    public void pushback(int n) {
+        if (length == capacity) this.resize();
+
+        array[length] = n;
+        length++;
+    }
+
+    public int popback() {
+        if (length > 0) length--;
+        return array[length];
+    }
+
+    private void resize() {
+        capacity *= 2;
+        int[] resizedArr = new int[capacity];
+        for (int i = 0; i < length; i++) {
+            resizedArr[i] = array[i];
+        }
+        array = resizedArr;
+    }
+
+    public int getSize() {
+        return length;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
